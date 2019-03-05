9c2ed3f2ee32801c39713ab60ddd49e90db4070b
eliemugenzi <eliemugenzi@gmail.com>
2019-03-06 01:33:02 +0200

    Change router config structure

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import 'dotenv/config';
-import logger from 'morgan';
-import router from './server/routes';
+import express from "express";
+import bodyParser from "body-parser";
+import "dotenv/config";
+import logger from "morgan";
+//import router from './server/routes';
+
+import authRouter from "./server/routes/auth";
+import messageRouter from "./server/routes/messages";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-app.get('/', (req, res) => res.json({ message: 'Hello world' }));
-app.use('/api/v1', router);
+app.get("/", (req, res) => res.json({ message: "Hello world" }));
+
+//Routers middleware config
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/messages", messageRouter);
 
 app.listen(process.env.PORT, () => {
   /* eslint-disable-next-line */
