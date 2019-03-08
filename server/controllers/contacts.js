a378c0c7186a99ffa83b9fb9ed3e02a300ce2aeb
eliemugenzi <eliemugenzi@gmail.com>
2019-03-08 17:22:37 +0200

    [feature-164461317] Remove class based models in contacts controller

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -1,17 +1,22 @@
-import Contact from "../models/contacts";
+import contacts from "../models/contacts";
 import jwt from "jsonwebtoken";
-
+import nmoment from "moment";
+import moment from "moment";
+import path from "path";
+import fs from "fs";
 class ContactController {
   static contacts = (req, res) => {
     res.json({
       status: 200,
-      data: Contact.findAll()
+      data: contacts
     });
   };
 
   static contact = (req, res) => {
     let { id } = req.params;
-    const contactById = Contact.findById({ id });
+    const contactById = contacts.find(
+      contact => parseInt(contact.id) === parseInt(id)
+    );
     if (contactById) {
       let context = {
         status: 200,
@@ -28,7 +33,19 @@ class ContactController {
 
   static createContact = (req, res) => {
     let { firstname, lastname, email } = req.body;
-    let newContact = Contact.save({ email, firstname, lastname });
+    let newContact = {
+      id: contacts.length + 1,
+      firstname,
+      lastname,
+      email,
+      createdOn: moment().format("LL")
+    };
+
+    contacts.push(newContact);
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/contacts.json"),
+      JSON.stringify(contacts, null, 2)
+    );
     res
       .json({
         status: 201,
