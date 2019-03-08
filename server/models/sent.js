83277f38ba98c07a809f4fcabf3571129abb4974
eliemugenzi <eliemugenzi@gmail.com>
2019-03-08 16:14:20 +0200

    Remove class based models

--- server/models/sent.js
+++ server/models/sent.js
@@ -9,30 +9,4 @@ const sentData = fs.readFileSync(path.resolve(__dirname, "../data/sent.json"), {
 });
 sent = JSON.parse(sentData);
 
-//export default sent;
-
-class Sent {
-  constructor({ senderId, messageId }) {
-    this.senderId = senderId;
-    this.messageId = messageId;
-  }
-
-  static save({ senderId, messageId }) {
-    let newSent = {
-      senderId: senderId,
-      messageId: messageId,
-      createdOn: moment().format("LL")
-    };
-    sent.push(newSent);
-    fs.writeFileSync(
-      path.resolve(__dirname, "../data/sent.json"),
-      JSON.stringify(sent, null, 2)
-    );
-  }
-
-  static findBySender({ senderId }) {
-    return sender.filter(s => parseInt(s.senderId) == parseInt(senderId));
-  }
-}
-
-export default Sent;
+export default sent;
