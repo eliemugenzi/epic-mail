23140a652297eedec815d505231a470d3fb470ef
eliemugenzi <eliemugenzi@gmail.com>
2019-03-09 09:19:50 +0200

    Remove class based controller

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,20 +1,32 @@
 import { Router } from "express";
-import MessageController from "../controllers/messages";
+import {
+  messages,
+  message,
+  unread,
+  sent,
+  draft,
+  replyMessage,
+  createMessage,
+  moveToTrash,
+  allUnread,
+  allDrafts,
+  allRead
+} from "../controllers/messages";
 import verifyToken from "../middleware/auth";
 
 const router = Router();
 
-router.get("/", verifyToken, MessageController.messages);
-router.get("/unread", verifyToken, MessageController.unread);
-router.get("/sent", verifyToken, MessageController.sent);
-router.get("/draft", verifyToken, MessageController.draft);
-router.get("/:id", verifyToken, MessageController.message);
-router.post("/reply/:messageId", verifyToken, MessageController.replyMessage);
+router.get("/", verifyToken, messages);
+router.get("/unread", verifyToken, unread);
+router.get("/sent", verifyToken, sent);
+router.get("/draft", verifyToken, draft);
+router.get("/:id", verifyToken, message);
+router.post("/reply/:messageId", verifyToken, replyMessage);
 
-router.post("/", verifyToken, MessageController.createMessage);
-router.delete("/:messageId", verifyToken, MessageController.moveToTrash);
-router.get("/unread/messages", MessageController.allUnread);
-router.get("/draft/messages", MessageController.allDrafts);
-router.get("/read/messages", MessageController.allRead);
+router.post("/", verifyToken, createMessage);
+router.delete("/:messageId", verifyToken, moveToTrash);
+router.get("/unread/messages", allUnread);
+router.get("/draft/messages", allDrafts);
+router.get("/read/messages", allRead);
 
 export default router;
