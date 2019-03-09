44117a4814efc853d1ed49c5d9121b65cc859de9
eliemugenzi <eliemugenzi@gmail.com>
2019-03-09 18:45:45 +0200

    [feature-164461317] Removing unnecessary controllers and add class based controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,10 @@
 import users from "../models/users";
 import jwt from "jsonwebtoken";
 import validateUser from "../helpers/validations/user";
-import path from 'path';
-import fs from 'fs';
-
-
-  export const register = (req, res) => {
+import path from "path";
+import fs from "fs";
+class AuthController {
+  static register = (req, res) => {
     let { firstname, lastname, email, password } = req.body;
     const { error } = validateUser(req.body);
     if (error) {
@@ -15,14 +14,17 @@ import fs from 'fs';
       });
     }
     let newUser = {
-      id: users.length+1
+      id: users.length + 1,
       firstname,
       lastname,
       email,
       password
     };
     users.push(newUser);
-    fs.writeFileSync(path.resolve(__dirname, "../data/messages.json"), JSON.stringify(users));
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/messages.json"),
+      JSON.stringify(users)
+    );
     jwt.sign(
       {
         user: newUser
@@ -41,7 +43,7 @@ import fs from 'fs';
     );
   };
 
-  export const login = (req, res) => {
+  static login = (req, res) => {
     let { email, password } = req.body;
     let credentials = { email, password };
 
@@ -67,38 +69,13 @@ import fs from 'fs';
           error: "Invalid email and password combination"
         });
       }
-    }
-    else {
+    } else {
       res.status(404).json({
         status: 404,
-        error:'This user not found'
-      })
-    }
-    
-  };
-
-  export const users = (req, res) => {
-    let context = {
-      status: 200,
-      data: users
-    };
-    res.json(context);
-  };
-  export const singleUser = (req, res) => {
-    let { id } = req.params;
-    let singleUser = users.find(user => parseInt(user.id) === parseInt(id));
-    if (singleUser) {
-      res.json({
-        status: 200,
-        data: [singleUser]
-      });
-    } else {
-      res.status(400).json({
-        status: 400,
-        error: `This user of id ${id} is not found!`
+        error: "This user not found"
       });
     }
   };
+}
 
-
-
+export default AuthController;
