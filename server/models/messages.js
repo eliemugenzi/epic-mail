149182f2c9bbe45875d60cb2c495460d69cce8b5
eliemugenzi <eliemugenzi@gmail.com>
2019-03-15 11:42:19 +0200

    Add sent messages controller

--- server/models/messages.js
+++ server/models/messages.js
@@ -2,11 +2,58 @@ import path from 'path';
 import fs from 'fs';
 
 
-let messages = [];
-const messageData = fs.readFileSync(
-  path.resolve(__dirname, '../data/messages.json'),
-  { encoding: 'utf-8' },
-);
-messages = JSON.parse(messageData);
+let messages = [
+  {
+    id: 1,
+    senderId: 1,
+    receiverId: 2,
+    subject: "Welcome to the bootcamp cycle 4",
+    message: "Hey guys,welcome to the fourth Andela bootcamp edited",
+    parentMessageId: 0,
+    createdOn: "March 14, 2019",
+    status: "sent"
+  },
+  {
+    id: 2,
+    senderId: 1,
+    receiverId: 2,
+    subject: "Welcome to the bootcamp cycle 4 Edited",
+    message: "Hey guys,welcome to the fourth Andela bootcamp edited",
+    parentMessageId: 0,
+    createdOn: "March 14, 2019",
+    status: "read"
+  },
+  {
+    id: 3,
+    senderId: 1,
+    receiverId: 2,
+    subject: "Welcome to the bootcamp cycle 4 Edited Draft",
+    message: "Hey guys,welcome to the fourth Andela bootcamp edited",
+    parentMessageId: 0,
+    createdOn: "March 14, 2019",
+    status: "draft"
+  },
+  {
+    id: 4,
+    senderId: 1,
+    receiverId: 2,
+    subject: "Experiences in bootcamp",
+    message:
+      "Yesterday, we wrote about our experience gained from this bootcamp",
+    parentMessageId: 0,
+    createdOn: "March 14, 2019",
+    status: "read"
+  },
+  {
+    id: 5,
+    senderId: 2,
+    receiverId: 1,
+    subject: "Welcome to the bootcamp cycle 4 Edited Draft",
+    message: "Hey guys,welcome to the fourth Andela bootcamp edited",
+    parentMessageId: 0,
+    createdOn: "March 14, 2019",
+    status: "draft"
+  }
+];
 
 export default messages;
