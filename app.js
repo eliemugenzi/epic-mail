09eab2a11115080fabdb133008b4b1e6a9bac1f0
eliemugenzi <eliemugenzi@gmail.com>
2019-03-19 20:38:49 +0200

    [chore-164679255] Link new routes and add init-db script

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import 'dotenv/config';
-import logger from 'morgan';
+import express from "express";
+import bodyParser from "body-parser";
+import "dotenv/config";
+import logger from "morgan";
 
 
-import authRouter from './server/routes/auth';
-import messageRouter from './server/routes/messages';
-import userRouter from './server/routes/users';
-import contactRouter from './server/routes/contacts';
+import authRouter from "./server/routes/auth";
+import messageRouter from "./server/routes/messages";
+import userRouter from "./server/routes/users";
+import contactRouter from "./server/routes/contacts";
+import groupRouter from "./server/routes/groups";
+
+import Db from "./server/db";
 
 const app = express();
 
@@ -15,21 +18,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-app.get('/', (req, res) => res.json({ message: 'Hello world' }));
+app.get("/", (req, res) => res.json({ message: "Hello world" }));
 
-//Routers middleware config
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/messages', messageRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/contacts', contactRouter);
+// Routers middleware config
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/messages", messageRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/contacts", contactRouter);
+app.use("/api/v1/groups", groupRouter);
 
-app.get('*', (req, res) => res.status(404).json({ status: 404, error: 'The endpoint is not found' }));
-
-app.listen(process.env.PORT, () => {
+app.get("*", (req, res) => res.status(404).json({ status: 404, error: "404 not found" }));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   /* eslint-disable-next-line */
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
 
 export default app;
