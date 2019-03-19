73d6d70024a79b9a5998ce17483fb80bfccac349
eliemugenzi <eliemugenzi@gmail.com>
2019-03-19 20:36:30 +0200

    [chore-164679255] Add routes for challenge 3

---
+++ server/routes/groups.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import verifyToken from "../middleware/auth";
+import GroupController from "../controllers/groups";
+
+const router = Router();
+
+router.get("/", GroupController.groups);
+router.post("/", verifyToken, GroupController.createGroup);
+router.patch("/:id", verifyToken, GroupController.editName);
+router.delete("/:id", verifyToken, GroupController.deleteGroup);
+router.post("/:groupId/users", verifyToken, GroupController.addMembers);
+router.delete("/:groupId/users/:memberId", GroupController.deleteUser);
+router.post("/:groupId/messages", verifyToken, GroupController.sendGroupMessage);
+
+export default router;
--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,23 +1,28 @@
-import { Router } from 'express';
-import MessageController from '../controllers/messages';
-import verifyToken from '../middleware/auth';
-import { messageValidate } from '../middleware/validations';
+import { Router } from "express";
+import MessageController from "../controllers/messages";
+import verifyToken from "../middleware/auth";
+import { messageValidate } from "../middleware/validations";
 
 const router = Router();
 
-router.get('/', verifyToken, MessageController.userMessages);
-router.get('/unread', verifyToken, MessageController.unread);
-router.get('/sent', verifyToken, MessageController.sentMsg);
-router.get('/draft', verifyToken, MessageController.draft);
-router.get('/:id', verifyToken, MessageController.message);
-router.post('/reply/:messageId', verifyToken, messageValidate, MessageController.replyMessage);
+router.get("/", verifyToken, MessageController.userMessages);
+router.get("/unread", verifyToken, MessageController.unread);
+router.get("/sent", verifyToken, MessageController.sentMsg);
+router.get("/draft", verifyToken, MessageController.draft);
+router.get("/:id", verifyToken, MessageController.message);
+router.post(
+  "/reply/:messageId",
+  verifyToken,
+  messageValidate,
+  MessageController.replyMessage,
+);
 
-router.post('/', verifyToken, messageValidate, MessageController.createMessage);
-router.delete('/:messageId', verifyToken, MessageController.moveToTrash);
-router.get('/unread/messages', MessageController.allUnread);
-router.get('/draft/messages', MessageController.allDrafts);
-router.get('/sent/messages', MessageController.allSent);
-router.get('/read/messages', MessageController.allRead);
-router.get('/public/:id', MessageController.singlePublicMsg);
+router.post("/", verifyToken, messageValidate, MessageController.createMessage);
+router.delete("/:messageId", verifyToken, MessageController.moveToTrash);
+router.get("/unread/messages", MessageController.allUnread);
+router.get("/draft/messages", MessageController.allDrafts);
+router.get("/sent/messages", MessageController.allSent);
+router.get("/read/messages", MessageController.allRead);
+router.get("/public/:id", MessageController.singlePublicMsg);
 
 export default router;
