7d1ee7403a04956beca4778e96a3a4e4f427d460
eliemugenzi <eliemugenzi@gmail.com>
2019-03-19 21:02:28 +0200

    [feature-164679255] Creating a registration controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,6 +5,45 @@ import bcrypt from "bcryptjs";
 import Db from "../db";
 class AuthController {
 
+  static register(req, res) {
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+    } = req.body;
+    const sql = `SELECT * FROM users WHERE email='${email}'`;
+    Db.query(sql).then((result) => {
+      console.log(result.rows);
+      if (result.rows.length) {
+        return res.status(400).json({
+          status: 400,
+          error: "The user with this email already exists",
+        });
+      }
+
+      const newUser = [
+        firstname,
+        lastname,
+        email,
+        password,
+        new Date(),
+      ];
+      const sql2 = "INSERT INTO users(firstname,lastname,email,password,createdon) VALUES($1,$2,$3,$4,$5) RETURNING *";
+      Db.query(sql2, newUser).then((result) => {
+        console.log(result.rows);
+        jwt.sign({ user: { email, password } }, process.env.SECRET_KEY, (err, token) => {
+          res.status(201).json({
+            status: 201,
+            data: [{ token }],
+          });
+        });
+
+      });
+    })
+
+  }
+
   static login(req, res) {
     const { email, password } = req.body;
     const credentials = { email, password };
